235434c79e099f7d642b484b96eba13702437019
Misa (Golem) <misaelmonero4c08@gmail.com>
2020-11-04 23:25:16 -0400

    routes video 2

--- HolaMundo/src/app/app.module.ts
+++ HolaMundo/src/app/app.module.ts
@@ -1,23 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from  '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './Property/Property-Card/property-card.component';
 import { PropertyListComponent } from './Property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import {  HousingService } from  './services/housing.service';
+import { HousingService } from './services/housing.service';
+import { AddPropertyComponent } from './Property/add-property/add-property.component';
+import { PropertyDetailComponent } from './Property/property-detail/property-detail.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: PropertyListComponent },
+  { path: 'rent-property', component: PropertyListComponent },
+  {  path:'property-detail/:id', component:PropertyDetailComponent},
+  { path: 'add-property', component: AddPropertyComponent }
+
+]
 
 @NgModule({
   declarations: [
+    AddPropertyComponent,
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
-      NavBarComponent
-   ],
+    NavBarComponent,
+    PropertyDetailComponent
+  ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     HousingService
